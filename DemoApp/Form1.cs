b6c283fe298d49afa059473abfd917d015c7c963
sh4d0w4RCH3R415 <misterjeremiahjordan@live.com>
2022-04-02 21:22:07 -0700

    Removed un-used Section/Entry class and added new AddSection(string) method for whatever purpose you want

--- DemoApp/Form1.cs
+++ DemoApp/Form1.cs
@@ -11,13 +11,7 @@ namespace DemoApp
 
 			config = new Ini(Application.StartupPath + "\\config.ini");
 
-			config.ChangeSectionName("Example-Settings", "Example Settings");
-			config.ChangeEntryName("Example Settings", "Example-1", "Example 10");
-			config.ChangeEntryName("Example Settings", "Example 2", "Example-20");
-
-			config.ChangeSectionName("Example-Settings-2", "Example Settings 2");
-			config.ChangeEntryName("Example Settings 2", "Example-1_2", "Example 10_2");
-			config.ChangeEntryName("Example Settings 2", "Example 2_2", "Example-20_2");
+			config.AddSection("Example-Settings-3");
 		}
 
 		private Ini config = null;
